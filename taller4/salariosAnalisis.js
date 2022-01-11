d5476511ce1bd62a982387cc1d293facadc6c4b3
fruiza <ceo.texcomercial@texcomercial.com.co>
2022-01-10 21:42:58 -0500

    creación de archivo 'salariosAnalisis.js'

---
+++ taller4/salariosAnalisis.js
@@ -0,0 +1,62 @@
+function displayResults(id, text) {
+    const element = document.getElementById(id);
+    const resultsDisplayed = element.innerHTML
+    if (resultsDisplayed) {    
+        element.innerText= "";
+        element.innerText = text 
+    } else {
+        element.innerText = text 
+    }
+}
+
+function getInputText () {
+    const basicWage = document.getElementById("input-wage")?document.getElementById("input-wage").value:0;
+    const dayExtraHours =  document.getElementById("input-day-extra-hours")?document.getElementById("input-day-extra-hours").value:0;
+    const sundayExtraHours = document.getElementById("input-sunday-extra-hours")?document.getElementById("input-sunday-extra-hours").value:0;
+    const holidayExtraHours =  document.getElementById("input-holiday-extra-hours")?document.getElementById("input-holiday-extra-hours").value:0;
+
+    basicWage = parseInt(basicWage);
+    dayExtraHours = parseInt(dayExtraHours);
+    sundayExtraHours = parseInt(sundayExtraHours);
+    holidayExtraHours = parseInt(holidayExtraHours);
+    console.log(basicWage + dayExtraHours + sundayExtraHours + holidayExtraHours );
+    return basicWage, dayExtraHours, sundayExtraHours, holidayExtraHours
+};
+
+function getHourlyWage(wage) {
+    return wage/240;
+};
+
+function calculateDayExtraHoursEarnings (hourlyWage, extraHours) {
+    return 1.5 * hourlyWage * extraHours
+};
+
+function calculateSundayExtraHoursEarnings (hourlyWage, extraHours) {
+    return 1.6 * hourlyWage * extraHours
+};
+
+function calculateHolidayExtraHoursEarnings (hourlyWage, extraHours) {
+    return 1.75 * hourlyWage * extraHours
+};
+
+function calculateTotalEarnings () {
+    const basicWage, dayExtraHours, sundayExtraHours, holidayExtraHours = getInputText();
+    const hourlyWage = getHourlyWage(basicWage);
+    const dayExtraHoursEarnings = calculateDayExtraHoursEarnings(hourlyWage, dayExtraHours);
+    const sundayExtraHoursEarnings = calculateSundayExtraHoursEarnings(hourlyWage, sundayExtraHours);
+    const holidayExtraHoursEarnings =  calculateHolidayExtraHoursEarnings(hourlyWage, holidayExtraHours);
+
+    const totalEarnings = basicWage + dayExtraHoursEarnings + sundayExtraHoursEarnings + holidayExtraHoursEarnings
+
+    const message = `Tu salario por hora es $${hourlyWage}. 
+    
+    Tus horas extras trabajadas fueron ${dayExtraHours} y ganaste $${dayExtraHoursEarnings}.
+    Tus horas dominicales trabajadas fueron ${sundayExtraHours} y ganaste $${sundayExtraHoursEarnings}.
+    Tus horas festivas trabajadas fueron ${holidayExtraHours} y ganaste $${holidayExtraHoursEarnings}.
+    
+    Tu salario total es $${totalEarnings}.`
+
+    displayResults("result-parragraph", message)
+
+
+};
