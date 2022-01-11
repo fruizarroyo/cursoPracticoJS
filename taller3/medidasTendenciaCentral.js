a8e34e7c41c93ba5236ba68af2c3a345ab75e04f
fruiza <ceo.texcomercial@texcomercial.com.co>
2022-01-10 20:33:29 -0500

    diseño de página 'medidas de tendencia central' terminado

--- taller3/medidasTendenciaCentral.js
+++ taller3/medidasTendenciaCentral.js
@@ -1,25 +1,73 @@
-function geometricMean (array) {
 
+function displayResults(result, id, text) {
+    const element = document.getElementById(id);
+    const resultsDisplayed = element.innerHTML
+    if (resultsDisplayed) {    
+        element.innerText= "";
+        element.innerText = text +": " + result; 
+    } else {
+        element.innerText = text +": " + result; 
+    }
+}
+
+function convertToInt (array) {
+    return array.map( (element) => parseInt(element))
+}
+
+
+function geometricMean () {
+    const array = inputArray;
     let growthFactor = array.map( (element) => element+1)
     let n = growthFactor.length;
     const formula =  (growthFactor.reduce( (a,b) => a*b))**(1/n);
     const result = (formula-1)*100;
-    return result;
+    displayResults(result, "result-parragraph", "La media geométrica del array de valores es")
 }
 
-function calculateMean(list) {
+
+
+function calculateMean(arrayParameter = null) {
     // let accumulatedSum = 0;
     // for (let i=0; i<list.length; i++) {
     //     accumulatedSum=accumulatedSum + list[i];
     // };
 
-    const accumulatedSum = list.reduce(
-        (accumulatedValue, currentValue)=>{
-            return accumulatedValue+currentValue;
-        })
+    // Declaramos el array del ámbito interno de la función
+    let array; 
 
-    const listMean = accumulatedSum/list.length;
-    return listMean;
+    //Comprobamos si se pasó un array como argumento
+    if (arrayParameter) {
+        array = arrayParameter;
+
+        array = convertToInt(array);
+
+        const accumulatedSum = array.reduce(
+            (accumulatedValue, currentValue)=>{
+                return accumulatedValue+currentValue;
+            })
+
+        const listMean = accumulatedSum/array.length;
+
+        return listMean;
+
+    } else {
+        const stringArray = document.getElementById("input-array").value;
+        array = stringArray.split(",");
+        
+    // Convertir a número cada elemento del array
+        array = convertToInt(array);
+
+        const accumulatedSum = array.reduce(
+            (accumulatedValue, currentValue)=>{
+                return accumulatedValue+currentValue;
+            })
+
+        const listMean = accumulatedSum/array.length;
+        displayResults(listMean, "result-parragraph", "La media del array de valores es")
+    }
+     
+
+
 };
 
 function isEven(number) {
@@ -31,31 +79,43 @@ function isEven(number) {
 };
 
 
-function calculateMedian (list) {
+function calculateMedian () {
+    const stringArray = document.getElementById("input-array").value;
+    let array = stringArray.split(",");
 
-    if (list.length) {
-        list.sort((a, b) => a-b) ;
+    // Convertir a número cada elemento del array
+    array = convertToInt(array);
 
-        const listMiddle = parseInt(list.length/2);
+    if (array.length) {
+        array.sort((a, b) => a-b) ;
+
+        const listMiddle = parseInt(array.length/2);
         let median;
 
-        if (isEven(list.length)) {
-            const firstElement = list[listMiddle-1];
-            const secondElement = list[listMiddle];
+        if (isEven(array.length)) {
+            const firstElement = array[listMiddle-1];
+            const secondElement = array[listMiddle];
             const mean = calculateMean([firstElement, secondElement]);
+
             median = mean;
         } else {
-            median = list[listMiddle];
+            median = array[listMiddle];
         };
-
-        return median;}
+        displayResults(median, "result-parragraph", "La mediana del array de valores es")
+    }
     else {
             console.error("La lista de datos debe contener al menos un valor");
         }
 
 };
 
-function calculateMode(array) {
+function calculateMode() {
+    const stringArray = document.getElementById("input-array").value;
+    let array = stringArray.split(",");
+
+    // Convertir a número cada elemento del array
+    array = convertToInt(array);
+
     let objectArray = {};
     //Convertir a objeto
     array.forEach( (arrayElement) => {
@@ -74,8 +134,16 @@ function calculateMode(array) {
         return frequency=== sortedArray[0][1] //Acceder al segundo elemento de la primera entrada.
     } );
 
+    // Extraer solo las modas de la lista de array filtrada
+    const modeArray = filteredArray.map( (element) => element[0]);
 
-    return filteredArray;
+    let mensaje;
+    if (filteredArray.length>1) {
+        mensaje = "Las modas del array de valores son" 
+    } else {
+        mensaje = "La moda del array de valores es" 
+    }
+    displayResults(modeArray.toString(), "result-parragraph",  mensaje)
 }
 
 
